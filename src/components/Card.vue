f64129b4c91a2cda9ee7e51476f4e0ba98114694
YunChienLou <47598862+YunChienLou@users.noreply.github.com>
2023-03-27 18:06:33 +0800

    first commit

---
+++ src/components/Card.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+      
+div.w-full.max-w-sm.bg-white.border.border-gray-200.rounded-lg.shadow
+      div.flex.flex-col.items-center.pb-4
+            img(src=`${imgUrl}` alt="Bonnie image").w-24.h-24.mb-3.rounded-full.shadow-lg
+            div.flex
+                  button.bg-transparent.text-gray-500.font-semibold.p-1.border.border-gray-500.rounded.mr-3.my-3
+                        svg(fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true").w-6.h-6
+                              path(stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z")
+                  div
+                        h5.mb-1.text-xl.font-medium.text-gray-900 {{ gender }}
+                        span.text-sm.text-gray-500 {{ name }}
+</template>
+<script setup>
+import { defineProps } from "vue";
+const props = defineProps({
+  imgUrl: { type: String, required: false },
+  name: { type: String, required: true },
+  gender: { type: String, required: true },
+});
+</script>
---
+++ src/components/SearchResult.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+div.grid.grid-cols-5.gap-4.m-4
+      Card(v-for="user in users" :imgUrl="user.picture.thumbnail" :name="user.name.first+' '+user.name.last" :gender="user.gender")
+</template>
+
+<script setup>
+import Card from "./Card.vue";
+import { inject, onMounted, ref } from "vue";
+const axios = inject("axios");
+const users = ref();
+
+onMounted(() => {
+  axios.get("https://randomuser.me/api/?results=10").then((res) => {
+    users.value = res.data.results;
+  });
+});
+</script>
